08310a990602ab9c1e7f5ce297c01ca90ecf1dd4
Sameer Adhikari <tri2sing@yahoo.com>
2014-07-02 16:48:20 -0700

    Added binary search on rotated array

--- src/ArrayProblems.java
+++ src/ArrayProblems.java
@@ -3,6 +3,38 @@ import java.util.HashSet;
 public class ArrayProblems {
 
 	/*
+	 * Finds whether a specified element exists in a sorted array that has
+	 * been rotated unknown number of times and does not have duplicates.
+	 * 
+	 * @param arr an array that has sorted elements rotated some times
+	 * @param target the element to find in the array
+	 * @return the location of the element if found, -1 otherwise
+	 */
+	
+	public static int binarySearchRotatedArray(int [] arr, int target) {
+		
+		int L = 0;
+		int R = arr.length - 1;
+		while (L <= R) {
+			int M = L + (R - L)/2;
+			if(arr[M] == target) return M;
+			if (arr[L] <= arr[M]) {  // Left half is sorted
+				if (arr[L] <= target && target < arr[M])
+					R = M - 1;
+				else
+					L = M + 1;
+			}
+			else {  // Right half is sorted
+				if (arr[M] <= target && target <= arr[R])
+					L = M + 1;
+				else
+					R = M - 1;
+			}
+		}
+		return -1;
+		
+	}
+	/*
 	 * Prints out the elements of an array that have the specified difference.
 	 * 
 	 * @param arr an array containing integers to check for the difference
@@ -30,14 +62,30 @@ public class ArrayProblems {
 		System.out.println();
 	}
 	
+	public static void printArray(int[] arr) {
+		System.out.print("[");
+		for(int n: arr) {
+			System.out.print(n + " ");
+		}
+		System.out.print("]");
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*
 		int [] arr = {10, 20, 30, 40, 15};
 		ArrayProblems.findDiffExists(arr, 10);
 		ArrayProblems.findDiffExists(arr, -10);
 		ArrayProblems.findDiffExists(arr, 0);
 		ArrayProblems.findDiffExists(arr, -1);
+		*/
 
+		int[] arr1 = {7, 8, 9, 0, 1, 2, 3, 4, 5, 6};
+		System.out.print("Searching ");
+		ArrayProblems.printArray(arr1);
+		System.out.println("2 is located at " + ArrayProblems.binarySearchRotatedArray(arr1, 2));
+		System.out.println("10 is located at " + ArrayProblems.binarySearchRotatedArray(arr1, 10));
 	}
 
 }
