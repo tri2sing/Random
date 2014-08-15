0f3b85953dce4f76c640b73540df2676f23d4734
Sameer Adhikari <tri2sing@yahoo.com>
2014-08-15 11:19:26 -0700

    Completed graph decomposition problem

--- src/DecomposeGraph.java
+++ src/DecomposeGraph.java
@@ -1,3 +1,10 @@
+/**
+ * You are given a tree (a simple connected graph with no cycles). 
+ * You have to remove as many edges from the tree as possible to obtain a forest with the condition 
+ * that each connected component of the forest should contain an even number of vertices.
+ * Note: The tree in the input will be such that it can always be decomposed into components containing even number of nodes. 
+ */
+
 import java.util.*;
 
 public class DecomposeGraph {
@@ -20,8 +27,8 @@ public class DecomposeGraph {
 		visited = new boolean[V];
 	}
 
-	// The input assumes vertices are 1 ... N
-	// We store them as 0 ... N-1
+	// The input assumes vertices are 1 ... N, but we store them as 0 ... N-1
+	// This function can only be used once when initially creating the graph.
 	public void addEdge(int v, int w) {
 		this.v[edgesAdded] = v - 1;
 		this.w[edgesAdded] = w - 1;
@@ -37,14 +44,15 @@ public class DecomposeGraph {
 		G[v][w] = false;
 		G[w][v] = false;
 	}
-	
+
 	// Start at a vertex and traverse all connected vertices
-	// It returns the count of the
+	// It returns the count of the vertices that it reaches
+	// from the starting vertex.
 	public int traverse(int v) {
 		int count = 1;
-		
+
 		visited[v] = true;
-		for(int i = 0; i < V; i++) {
+		for (int i = 0; i < V; i++) {
 			if (i != v && G[v][i] && !visited[i]) {
 				count += traverse(i);
 			}
@@ -52,8 +60,8 @@ public class DecomposeGraph {
 		return count;
 	}
 
-	// Find connected components
-	// It returns the count of nodes in each connected component
+	// Determine all connected components and return
+	// the count of nodes in each connected component
 	public ArrayList<Integer> components() {
 		ArrayList<Integer> counts = new ArrayList<Integer>();
 
@@ -65,6 +73,31 @@ public class DecomposeGraph {
 				counts.add(traverse(i));
 		}
 		return counts;
+	}
+
+	// This function removes an edge from the graph one at a time.
+	// If the removal results in components with even number of,
+	// then it increments the count. Else, it adds the removed
+	// edge back to the graph and goes to the next edge.
+
+	public int decompose() {
+		int edges = 0;
+		for (int i = 0; i < E; i++) {
+			removeEdge(v[i], w[i]);
+			boolean odd = false;
+			ArrayList<Integer> l = components();
+			for (int k : l) {
+				if (k % 2 != 0) {
+					odd = true;
+					G[v[i]][w[i]] = true;
+					G[w[i]][v[i]] = true;
+					break;
+				}
+			}
+			if (!odd)
+				edges++;
+		}
+		return edges;
 	}
 
 	public static void main(String[] args) {
@@ -79,11 +112,13 @@ public class DecomposeGraph {
 			int w = in.nextInt();
 			dg.addEdge(v, w);
 		}
-		
-		ArrayList<Integer> l = dg.components();
-		for(int i: l) {
-			System.out.println(i);
-		}
+
+		/*
+		 * ArrayList<Integer> l = dg.components(); for (int i : l) {
+		 * System.out.println(i); }
+		 */
+
+		System.out.println(dg.decompose());
 
 		in.close();
 	}
