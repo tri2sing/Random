e3f76406c4e95414a5cd3409d7ceb90013b3c3a7
Sameer Adhikari <tri2sing@yahoo.com>
2014-08-19 16:52:24 -0700

    Added a HackerRank template to read input

---
+++ src/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		for (int i = 0; i < T; i++) {
+			int N = in.nextInt();
+		}
+		in.close();
+	}
+
+}
