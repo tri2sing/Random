b1fbea1209030ccad18073efe6c588c56dea4ca4
Sameer Adhikari <sameer.adhikari@intel.com>
2014-10-17 07:30:50 -0700

    Solved the problem on cutting sticks down to zero

---
+++ SticksCut.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+/**
+ * 
+ * @author Sameer Adhikari
+ * Input: Unsorted array of integers
+ * Operation: In each round reduce the value of all integers by the minimum non-zero value
+ * Output: Number of values that can be reduced in each round till all values are zero
+ * Key: To reduce search for minimum element, sort the list ascending before starting cuts
+ *
+ */
+public class SticksCut {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		int N = in.nextInt();
+		int [] array = new int [N];
+		for(int i = 0; i < N; i++)
+			array[i] = in.nextInt();
+		Arrays.sort(array);
+		int start = 0;
+		while (start < N) {
+			int size = array[start];
+			int cuts = 0;
+			for (int j = start; j < N; j++) {
+				array[j] -= size;
+				cuts++;
+				if(array[j] == 0) start++;
+			}
+			System.out.println(cuts);
+		}
+	}
+
+}
