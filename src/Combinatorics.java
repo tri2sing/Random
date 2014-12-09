0e0f73ef25a73933792b52233edb98eb00171de1
Sameer Adhikari <sameer.adhikari@intel.com>
2014-12-08 20:40:37 -0800

    Added combinatorics -> combinations

--- src/Combinatorics.java
+++ src/Combinatorics.java
@@ -1,27 +1,78 @@
-import java.util.*;
-
 public class Combinatorics {
-	
 	/**
 	 * Function to implement n choose k.
+	 * 
 	 * @param n
 	 * @param k
 	 * @return
 	 */
-	public static int choose (int n, int k) {
+	public static int choose(int n, int k) {
 		int res = 1;
-		
+
 		for (int i = 0; i < k; i++) {
 			res *= (n - i);
 			res /= (i + 1);
 		}
-		
+
 		return res;
 	}
-	
+
+	/**
+	 * Generates all combinations of the characters in the given string. All the characters are distinct.
+	 * Note: It will generate duplicate results if input characters have repeats.
+
+	 * @param input the given input string.
+	 */
+	public static void combinations(String input) {
+		combinations(input, "");
+		System.out.println();
+	}
+
+	/**
+	 * Generates combinations of asked for length for the characters in the given string. All the characters are distinct.
+	 * Note: It will generate duplicate results if input characters have repeats.
+
+	 * @param input the given input string.
+	 */
+	public static void combinations(String input, int k) {
+		combinations(input, "", k);
+		System.out.println();
+	}
+
+	private static void combinations(String input, String prefix) {
+		int N = input.length();
+		if (!"".equals(prefix))
+			System.out.print(prefix + ", ");
+		for (int i = 0; i < N; i++) {
+			combinations(input.substring(i + 1), prefix + input.charAt(i));
+		}
+	}
+
+	private static void combinations(String input, String prefix, int k) {
+
+		if (k == 0)
+			System.out.print(prefix + ", ");
+
+		int N = input.length();
+		for (int i = 0; i < N; i++) {
+			combinations(input.substring(i + 1), prefix + input.charAt(i), k - 1);
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println ("4 choose 2 = " + Combinatorics.choose(4, 2));
-		System.out.println ("6 choose 2 = " + Combinatorics.choose(6, 2));
+		System.out.println("4 choose 2 = " + Combinatorics.choose(4, 2));
+		System.out.println("6 choose 2 = " + Combinatorics.choose(6, 2));
+
+		Combinatorics.combinations("abcd");
+		Combinatorics.combinations("abcd", 2);
+		Combinatorics.combinations("abcd", 3);
+		Combinatorics.combinations("abcd", 4);
+
+		Combinatorics.combinations("aaab");
+		Combinatorics.combinations("aaab", 2);
+		Combinatorics.combinations("aaab", 3);
+		Combinatorics.combinations("aaab", 4);
+
 	}
 
 }
